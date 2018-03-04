5ed33abd14e3362ae6d848d0c6f13941008e9dde
Bram van Eijk <bram@bramvaneijk.nl>
2018-03-04 01:34:48 +0100

    Update view list with error message

--- views/server/list.blade.php
+++ views/server/list.blade.php
@@ -7,7 +7,9 @@
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         Current b3 servers
-                        @can('manage', \Stormyy\B3\Models\B3Server::class)<a href="{{url('/b3/add')}}" class="btn btn-primary pull-right">Add server</a>@endcan
+                        @can('manage', \Stormyy\B3\Models\B3Server::class)<a href="{{url('/b3/add')}}"
+                                                                             class="btn btn-primary pull-right">Add
+                            server</a>@endcan
                     </div>
                     <div class="panel-body">
                         <table class="table table-striped">
@@ -21,18 +23,35 @@
                             <tbody>
                             @foreach($servers as $server)
                                 <tr>
-                                    <td>{{$server->name}} <a href="cod4://{{$server->host}}:{{$server->port}}" title="Join {{$server->name}} server"><i class="fa fa-sign-in"></i></a> </td>
+                                    <td>{{$server->name}} <a href="cod4://{{$server->host}}:{{$server->port}}"
+                                                             title="Join {{$server->name}} server"><i
+                                                    class="fa fa-sign-in"></i></a></td>
                                     <td>@if($server->slots == null)
                                             <font color="red">Offline</font>
                                         @else
-                                            <div class="progress">
-                                                <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="{{$server->online}}"
-                                                     aria-valuemin="0" aria-valuemax="{{$server->slots}}" style="width:{{round($server->online/$server->slots*100)}}%">
-                                                    {{$server->online}}/{{$server->slots}}
+                                            @if($server->online === -1 && $server->slots === -1)
+                                                <font color="red">Something is wrong</font>
+                                            @else
+                                                <div class="progress">
+                                                    <div class="progress-bar progress-bar-success progress-bar-striped active"
+                                                         role="progressbar" aria-valuenow="{{$server->online}}"
+                                                         aria-valuemin="0" aria-valuemax="{{$server->slots}}"
+                                                         style="width:{{round($server->online/$server->slots*100)}}%">
+                                                        {{$server->online}}/{{$server->slots}}
+                                                    </div>
                                                 </div>
-                                            </div>
+                                            @endif
                                         @endif </td>
-                                    <td style="text-align:right;"><a class="btn btn-primary" href="{{url('/b3/'.$server->id.'/players')}}">View</a> @can('manage', $server)<a class="btn btn-primary" href="{{url('/b3/'.$server->id.'/edit')}}">Edit</a>@endcan</td>
+                                    <td style="text-align:right;">
+                                        <a class="btn btn-primary" href="{{url('/b3/'.$server->id.'/players')}}">
+                                            View
+                                        </a>
+                                        @can('manage', $server)
+                                            <a class="btn btn-primary" href="{{url('/b3/'.$server->id.'/edit')}}">
+                                                Edit
+                                            </a>
+                                        @endcan
+                                    </td>
                                 </tr>
                             @endforeach
                             </tbody>
