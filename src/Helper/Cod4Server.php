2c0e7ee399dfff9f2fc78876d2939e70168cc4fa
Bram van Eijk <bram@bramvaneijk.nl>
2018-03-04 02:00:39 +0100

    Add error response if rcon can't be parsed

--- src/Helper/Cod4Server.php
+++ src/Helper/Cod4Server.php
@@ -14,6 +14,10 @@ use Stormyy\B3\Models\B3Server;
 
 abstract  class Cod4Server
 {
+    public static function getRcon(B3Server $server){
+
+    }
+
     public static function screenshotAll(B3Server $server){
         $tool = new q3tool($server->host, $server->port, \Crypt::decrypt($server->rcon));
         $response = $tool->send_rcon('getss all');
@@ -21,13 +25,20 @@ abstract  class Cod4Server
     }
 
     public static function screenshot(B3Server $server, $guid, $user=null){
-        $filename = '';
-        if($user != null){
-            $filename = ' user-'.$user->id.'-';
-        }
+        try {
+            $filename = '';
+            if ($user != null) {
+                $filename = ' user-' . $user->id . '-';
+            }
 
-        $tool = new q3tool($server->host, $server->port, \Crypt::decrypt($server->rcon));
-        $response = $tool->send_rcon('getss ' . $guid.$filename);
-        return $response;
+            $tool = new q3tool($server->host, $server->port, \Crypt::decrypt($server->rcon));
+            $response = $tool->send_rcon('getss ' . $guid . $filename);
+            return $response;
+        } catch (\Exception $exception){
+            return response()->json([
+                'code' => 'InvalidRcon',
+                'message' => 'Invalid rcon try reloading the nehoscreenshotuploader plugin'
+            ], 500);
+        }
     }
 }
